6c622f8ed4ce7495c6618817e3ec0d76ec17159e
unknown <jprudence@DESKTOP-H0S481E>
2021-04-24 23:23:31 +0000

    mon comit

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {NavigationNativeContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react'
 export default function App() {
   return (
     <View style={styles.container}>
